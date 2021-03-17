3308c4118e56ad53d4e62a4174c5184a7f33261a
HosodaMath <tomas0ark@gmail.com>
2021-03-17 12:07:43 +0900

    update

--- CreativeCording/webgl/primitive/polygon/polygonModel/Triangle/ColorsTriangle/p5js/script/main.js
+++ CreativeCording/webgl/primitive/polygon/polygonModel/Triangle/ColorsTriangle/p5js/script/main.js
@@ -1,29 +1,24 @@
-let sides = 3;
-let angle, px, py;
-
-
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
+    createCanvas(512, 512, WEBGL);
     noStroke();
-
-}
-
-function draw() {
-    background(0, 230, 100);
-    rotateX(frameCount * 0.01);
-    rotateZ(frameCount * 0.01);
-    ngon(sides, 0, 0, 512);
-}
-
-
-function ngon(n, x, y, d) {
-    //TESSは真ん中に穴を開ける
-    beginShape();
-    for (let i = 0; i < n + 1; i++) {
-        angle = TWO_PI / n * i;
-        px = x + sin(angle) * d / 2;
-        py = y - cos(angle) * d / 2;
-        vertex(px, py, 0);
-    }
-    endShape(CLOSE);
-}
+  }
+  
+  
+  let Triangle2 = () => {
+    push();
+    scale(100);
+    beginShape(TRIANGLES);
+    fill("#00ff00");
+    vertex(-1, 1, 0);
+    fill("#0000ff");
+    vertex(1, 1, 0);
+    fill("#00ffff");
+    vertex(0,-1,0);
+    endShape();
+    pop();
+  }
+  
+  function draw() {
+    background(0, 0, 0);
+    //Triangle2();
+  }
