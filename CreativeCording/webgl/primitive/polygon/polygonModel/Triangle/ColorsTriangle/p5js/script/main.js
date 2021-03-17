33712ada7966c9af41da5ce942cdd1b21b03e803
HosodaMath <tomas0ark@gmail.com>
2021-03-17 12:12:10 +0900

    update

--- CreativeCording/webgl/primitive/polygon/polygonModel/Triangle/ColorsTriangle/p5js/script/main.js
+++ CreativeCording/webgl/primitive/polygon/polygonModel/Triangle/ColorsTriangle/p5js/script/main.js
@@ -1,24 +1,39 @@
+class Triangle {
+    constructor(location1, velocity1, size1) {
+        this.location1 = location1;
+        this.velocity1 = velocity1;
+        this.size1 = size1;
+    }
+
+    drawStep() {
+        this.location1.add(this.velocity1);
+
+        if (this.location1 > height / 2.0) {
+            this.location1 = -height / 2.0;
+        }
+    }
+
+    drawTriangle(){
+        push();
+        scale(50);
+        beginShape(TRIANGLES);
+        fill("#00ff00");
+        vertex(-1, 1, 0);
+        fill("#0000ff");
+        vertex(1, 1, 0);
+        fill("#00ffff");
+        vertex(0, -1, 0);
+        endShape();
+        pop();
+    }
+}
+
 function setup() {
-    createCanvas(512, 512, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL);
     noStroke();
-  }
-  
-  
-  let Triangle2 = () => {
-    push();
-    scale(100);
-    beginShape(TRIANGLES);
-    fill("#00ff00");
-    vertex(-1, 1, 0);
-    fill("#0000ff");
-    vertex(1, 1, 0);
-    fill("#00ffff");
-    vertex(0,-1,0);
-    endShape();
-    pop();
-  }
-  
-  function draw() {
+}
+
+
+function draw() {
     background(0, 0, 0);
-    //Triangle2();
-  }
+}
